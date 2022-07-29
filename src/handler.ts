9081747b844483fb46a2d3b4b40c0a169ff2cb83
Marcos <marcos@debian-marcos>
2022-07-29 14:54:39 -0300

    Arreglos para ts

--- src/handler.ts
+++ src/handler.ts
@@ -1,6 +1,6 @@
 //const browserObject = require('./browser');
 
-import {start} from './browser.js';
+import {start} from './browser';
 
 export async function getBasicData(){
     let brouserInstance = await start();
@@ -11,44 +11,46 @@ export async function getBasicData(){
         return {
             'tag': 'temperatura actual',
             'value': elem.innerHTML,
-            'unit': elem.nextElementSibling.innerHTML
+            'unit': elem.nextElementSibling?.innerHTML
         }
     });
     let dataViento = await page.$eval("#valViento", elem => {
         return {
             'tag': 'viento',
             'value': elem.innerHTML,
-            'unit': elem.nextElementSibling.innerHTML
+            'unit': elem.nextElementSibling?.innerHTML
         }
     });
     let dataPresion = await page.$eval("#valPresion", elem => {
         return {
             'tag': 'presión',
             'value': elem.innerHTML,
-            'unit': elem.nextElementSibling.innerHTML
+            'unit': elem.nextElementSibling?.innerHTML
         }
     });
     let dataHumedad = await page.$eval("#valHumedad", elem => {
         return {
             'tag': 'humedad',
             'value': elem.innerHTML,
-            'unit': elem.nextElementSibling.innerHTML
+            'unit': elem.nextElementSibling?.innerHTML
         }
     });
     let dataVisibilidad = await page.$eval("#valVisibilidad", elem => {
         return {
             'tag': 'humedad',
             'value': elem.innerHTML,
-            'unit': elem.nextElementSibling.innerHTML
+            'unit': elem.nextElementSibling?.innerHTML
         }
     });
     let dataEstado = await page.evaluate(() => {
         let element = document.querySelector("#iconoGrandeEA");
+        //@ts-ignore
         let computed = window.getComputedStyle(element, "::after");
         console.log(element, computed);
         let result = Object.entries(computed).filter(style => style[0] == 'content');
         return {
             'tag': 'estado',
+        //@ts-ignore
             'value': result[0][1].replaceAll('"', '')
         }
     });
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import bodyParser from "body-parser";
 import express from "express";
-import { getBasicData } from './handler.js';
+import { getBasicData } from './handler';
 
 const app = express();
 const port = process.env.PORT || 3333;
